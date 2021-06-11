ba8653004683673a758e60cad80c8ced984a0f8e
Aarav619 <aaravmishra619@gmail.com>
2021-06-11 09:43:10 +0530

    update file

--- FULL_STACK/JavaScript/mongodb-node/app.js
+++ FULL_STACK/JavaScript/mongodb-node/app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+//module with external requests
+const userRoute = require("./Routes/UserRoutes")
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json());
 
 mongoose
-.connect("" ,{
-    useUnifiedTopology: true
-})
+  .connect("mongodb://localhost:27017/test", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.info("MongoDB connected successfully");
+  })
+  .catch(() => {
+    console.error("MongoDB connection failed.");
+  });
 
-
-
+app.use(userRoute);
 const PORT =8000;
 
 app.listen(PORT,() =>{
