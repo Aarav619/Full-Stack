4815bccaf27805e6bff93a35cad5a4716e8b38a4
Aarav619 <aaravmishra619@gmail.com>
2021-06-15 22:51:26 +0530

    [update] add try catch block

--- FULL_STACK/JavaScript/mongodb-node/middleware/IsTokenValid.js
+++ FULL_STACK/JavaScript/mongodb-node/middleware/IsTokenValid.js
@@ -1,14 +1,22 @@
-const express = require("express");
-const router = express();
 const JWT = require("jsonwebtoken");
 
-exports.isTokenValid = (req, res, next) => {
-    if (!req.headers.token) {
-        console.error("No token was sent");
-        return res.status(403).send("Invalid token");
+exports.isTokenValid = (req, res, next) => { //Callback argument to the middleware function, called "next" by convention.
+    try {
+        if (!req.headers.token) {
+            console.error("No token was sent");
+            return res.status(403).send("Invalid token");
+        }
+        //.verify(wheretoaccesstoken ,TokenKey)
+        const decodedToken = JWT.verify(req.headers.token, "TESTSecretKey");//Token verification with Provided key
+        if (decodedToken.email === req.body.email) {
+            return next();
+        }
+        console.warn("Please send valid token");
+        return res.status(417).send("Token Mismatch");
     }
-    //.verify(wheretoaccesstoken ,Key)
-    const decodedToken = JWT.verify(req.headers.token, "TESTSecretKey");//Token verification with Provided key
-    console.log(decodedToken);
-    next();
-}   
+
+    catch (error) {
+        console.error("Token validation failed.");
+        return res.status(401).send("Not a Token.");
+    }
+}
