48f3188f8271f46b5e08093bfd24e235713ace34
Aarav619 <aaravmishra619@gmail.com>
2021-06-15 08:51:47 +0530

    Update Shift to UserController

--- FULL_STACK/JavaScript/mongodb-node/Routes/UserRoutes.js
+++ FULL_STACK/JavaScript/mongodb-node/Routes/UserRoutes.js
@@ -1,60 +1,22 @@
 const express = require("express");
+//routes all requests made to server
+const router = express(); //retrieving external routes
+const controller = require("../Controllers/UserController")
 
-const router = express();
-const User = require("../model/User");
+//signup
+router.post("/signup", controller.SignupController)
+//login
+router.post("/login",controller.LoginUsingEmailPass );
+//searching using ID
+router.get("/user/:id", controller.SearchUsingId);
+//updating a user with email
+router.put("/update/user/:email", controller.UpdateUsingEmail);
 
+//updating a user with id
+// router.put("/update/:id", controller.UpdateUsingId);
 
-router.post("/signup", (req, res) => {
-  let { firstName, lastName, email, password } = req.body;
-  let user = new User({
-    firstName,
-    lastName,
-    email,
-    password,
-  });
-  user
-    .save()
-    .then(() => res.status(200).send(user))
-    .catch((error) => {
-      console.error(error);
-      return res.status(500).send("ERROR");
-    });
-});
-
-router.post("/login", (req, res) => {
-  let { email, password } = req.body;
-  User.findOne({ email: email })
-    .then((user) => {
-      console.info(`User with email ${email} sucessfully found.`);
-      if (password === user.password) {
-        console.info("login Successful");
-        return res.status(200).send(user);
-      }
-      console.warn("Password Incorrect!");
-      return res.status(401).send("Password was incorrect")
-    })
-    .catch((error) => {
-      console.error(`User with the email :${email} doesn't exits`);
-      return res.status(404).send(`User with the ${email} doesn't exits!`)
-    })
-})
-
-//updating a user
-router.put("/update/:email", (req, res) => {
-  let { email, password, firstName, lastName } = req.body;
-  let emailParam = req.params.email; //to retrieve email from path/route
-  //updateOne(1param ,2param)  1param:"where to update" 2 param :"what to update"
-  User.updateOne({ email: emailParam },
-    { $set: { email, password, firstName, lastName } }) //$set: internal command for mongodb to update
-
-    .then(() => {
-      console.info("Update Successful");
-      res.status(200).send({ email, password, firstName, lastName });
-    })
-    .catch((error) => {
-      console.error("There was an error while updating!");
-      res.status(500).send("There was an error while updating User!");
-    })
-})
+// retrieving through path Queries
+router.get("/get-User", controller.findUserbyfirstNameandlastName);
 
 module.exports = router;
+
