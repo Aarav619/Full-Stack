a564756cc0373ccb6c2f0ec8ce40c82acf07f94e
Aarav619 <aaravmishra619@gmail.com>
2021-06-19 20:27:41 +0530

    [update] add cors

--- FULL_STACK/JavaScript/mongodb-node/app.js
+++ FULL_STACK/JavaScript/mongodb-node/app.js
@@ -3,10 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 //module with external requests
 const userRoute = require("./Routes/UserRoutes")
+const cors = require("cors")
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json());
-
+app.use(cors());//for node to allow to get request from other ports
 mongoose
   .connect("mongodb://localhost:27017/test", {
     useUnifiedTopology: true,
